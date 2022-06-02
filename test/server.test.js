e03e4f9b0c2b203aeaa6f892885c53ba68295ca3
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-06-02 15:47:11 -0300

    Feat: add windows in CI

--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,10 @@
 const request = require("supertest");
-const app = require("./app");
+const app = require("../app");
 
 describe("GET / ", () => {
-  test("It should respond with an hello world", async () => {
+  test("should respond with an hello world", async () => {
     const response = await request(app).get("/");
     expect(response.text).toEqual('"Hello world"');
     expect(response.statusCode).toBe(200);
   });
-});
-
-test("should add two numbers", () => {
-  expect(1 + 4).toBe(5);
 });
